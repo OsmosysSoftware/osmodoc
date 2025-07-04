ce8a1d910570bc8e24de736bb72f194e031ff7fd
Jatin <jatin.g@osmosys.co>
2025-07-04 15:23:01 +0530

    feat: enable Swagger in Production environment

--- OsmoDoc.API/Program.cs
+++ OsmoDoc.API/Program.cs
@@ -155,7 +155,7 @@ Log.Logger = new LoggerConfiguration()
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
